cd7e99aa13ced79b6c102e4d9b7879c38d942894
tim <tim@brightsidedeveloper.com>
2024-09-13 21:20:56 -0500

    wtevr

--- src/api/Rpc.ts
+++ src/api/Rpc.ts
@@ -1,15 +1,9 @@
 import { GenerateFunctionsType } from '@/types/bright.types';
 import { BrightBaseFunctions } from 'bsdweb';
 
-const Functions: (keyof FunctionsType)[] = ['execute_sql', 'get_todos', 'search_todos_by_embedding'];
+const Functions: (keyof FunctionsType)[] = ['execute_sql', 'get_posts_with_like_count', 'get_todos', 'search_todos_by_embedding'];
 
-type Returns = {
-  execute_sql: unknown // Define the return type of the function
-  get_todos: unknown // Define the return type of the function
-  search_todos_by_embedding: unknown // Define the return type of the function
-};
-
-type FunctionsType = GenerateFunctionsType<Returns>;
+type FunctionsType = GenerateFunctionsType;
 
 const functions = new BrightBaseFunctions<FunctionsType>(Functions).functions;
 
--- src/types/bright.types.ts
+++ src/types/bright.types.ts
@@ -1,13 +1,14 @@
-import { Database } from './database.types';
-export type { Tables as BrightTable } from './database.types.ts';
+import { Database } from './database.types'
+export type { Tables as BrightTable } from './database.types.ts'
 // Utility types to infer Args and Returns from the Database's Functions type
-type FunctionArgs<F extends keyof Database['public']['Functions']> = Database['public']['Functions'][F]['Args'];
+type FunctionArgs<F extends keyof Database['public']['Functions']> = Database['public']['Functions'][F]['Args']
+type FunctionReturns<F extends keyof Database['public']['Functions']> = Database['public']['Functions'][F]['Returns']
 
 // Helper type to generate the FunctionsType
-export type GenerateFunctionsType<T extends Record<keyof Database['public']['Functions'], unknown>> = {
+export type GenerateFunctionsType = {
   [K in keyof Database['public']['Functions']]: {
-    args: FunctionArgs<K>;
-    returns: T[K];
+    args: FunctionArgs<K>
+    returns: FunctionReturns<K>
   }
 }
 
@@ -15,4 +16,5 @@ export interface RealtimeEvents {
   [event: string]: { [key: string]: unknown }
 }
 
-export type EventCallback<K extends RealtimeEvents, T extends keyof K> = (payload: K[T]) => void;
+export type EventCallback<K extends RealtimeEvents, T extends keyof K> = (payload: K[T]) => void
+
